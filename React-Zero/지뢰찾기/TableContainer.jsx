4ac307422872f0a2d9d0600126c104e9b917afb7
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-19 02:19:25 +0900

    TableContainer 구현 완료(테스트 통과)

--- React-Zero/지뢰찾기/TableContainer.jsx
+++ React-Zero/지뢰찾기/TableContainer.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import Table from './Table';
+
 export default function TableContainer() {
-	const dispatch = useDispatch();
-	const { tableData } = useSelector((state) => ({
-		tableData: state.tableData,
-	}))
+  const dispatch = useDispatch();
+  const { tableData } = useSelector((state) => ({
+    tableData: state.tableData,
+  }))
 
-	function handleClickCell() {
+  function handleClickCell(row, col) {
+    dispatch({ type: 'CLICK_CELL', row, col })
+  }
 
-	}
-
-	return <Table
-		tableData={tableData}
-		onClickCell={handleClickCell}
-	/>
+  return (
+    <Table
+      tableData={tableData}
+      handleClick={handleClickCell}
+    />
+  )
 }
