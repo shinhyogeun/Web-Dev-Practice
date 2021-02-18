424b291ba6f4acd523dfaa6b852c9e2b01a5b83b
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-18 23:36:09 +0900

    MindFind에 제목을 보여준다는 test추가

--- React-Zero/지뢰찾기/MindFind.test.jsx
+++ React-Zero/지뢰찾기/MindFind.test.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
 import MineFind from './MineFind'
 
 describe('지뢰찾기에서', () => {
-    it('..한다', () => {
-        
-    });
+  it('지뢰찾기 제목을 보여준다.', () => {
+    const { queryByText } = render(<MineFind />)
+
+    expect(queryByText('지뢰를 찾아봅시다!')).toBeInTheDocument();
+  });
 })
