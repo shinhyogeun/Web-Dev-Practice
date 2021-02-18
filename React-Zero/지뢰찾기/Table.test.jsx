f6740e3b7d14147d74e138fce559398406542b7b
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-18 23:24:40 +0900

    Table에 대한 test 추가

--- React-Zero/지뢰찾기/Table.test.jsx
+++ React-Zero/지뢰찾기/Table.test.jsx
@@ -5,5 +5,21 @@ import Table from './Table';
 import { render } from '@testing-library/react';
 
 describe('Table에서', () => {
-	it('')
+  const handleClick = jest.fn();
+
+  it('모든 Cell들을 잘 보여준다.', () => {
+    const tableData = [
+      [0, 0, 0, 1],
+      [1, 0, 1, 0],
+      [1, 0, 0, 1],
+      [0, 0, 1, 0]
+    ]
+    const { queryAllByText } = render(<Table tableData={tableData} onClick={handleClick} />);
+
+    [0, 1].forEach((value) => {
+      queryAllByText(value).forEach((cell) => {
+        expect(cell).toBeInTheDocument();
+      })
+    });
+  })
 })
