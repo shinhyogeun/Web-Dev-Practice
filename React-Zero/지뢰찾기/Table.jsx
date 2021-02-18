de412063a64c432e356e3c39a13121351c7e7abc
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-19 02:15:55 +0900

    Tr태그를 tbody로 감싸서 경고 해결

--- React-Zero/지뢰찾기/Table.jsx
+++ React-Zero/지뢰찾기/Table.jsx
@@ -5,14 +5,16 @@ import Tr from './Tr';
 export default function Table({ tableData, handleClick }) {
   return (
     <table>
-      {tableData.map((oneLine, index) => (
-        <Tr
-          key={String(oneLine.join('')) + String(index)}
-          row={index}
-          cols={oneLine}
-          onClick={handleClick}
-        />
-      ))}
+      <tbody>
+        {tableData.map((oneLine, index) => (
+          <Tr
+            key={String(oneLine.join('')) + String(index)}
+            row={index}
+            cols={oneLine}
+            onClick={handleClick}
+          />
+        ))}
+      </tbody>
     </table>
   )
 }
