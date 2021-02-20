bc14c5c6549e1205b0ec7b346615b89bd95ded13
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-20 20:48:32 +0900

    InputContainer test 작성

---
+++ React-Zero/지뢰찾기/InputContainer.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import InputContainer from './InputContainer';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+jest.mock('react-redux');
+
+describe('InputContainer', () => {
+  const dispatch = jest.fn();
+  const data = {
+    rowValue: '',
+    colValue: '',
+    mineCount: '',
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useSelector.mockImplementation((selector) => selector({ data }));
+    useDispatch.mockImplementation(() => (dispatch));
+  });
+
+  it('input을 입력하면 dispatch가 실행된다.', () => {
+    const { queryByPlaceholderText } = render(<InputContainer />)
+
+    const placeholders = ['row', 'col', 'mineCount'];
+
+    placeholders.forEach((placeholder) => {
+      fireEvent.change(queryByPlaceholderText(placeholder), {
+        target: {
+          value: 10
+        },
+      });
+
+      expect(dispatch).toBeCalled();
+    })
+  });
+
+  it('만들기 버튼을 클릭하면 dispatch가 실행된다.', () => {
+    const { queryByText } = render(<InputContainer />);
+
+    fireEvent.click(queryByText('만들기'));
+
+    expect(dispatch).toBeCalled();
+  });
+});
