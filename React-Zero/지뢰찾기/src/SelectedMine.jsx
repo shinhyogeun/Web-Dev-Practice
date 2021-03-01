756b96f4c212aa62928e7f0a5f7c2849d537f0d3
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-02 01:03:15 +0900

    selectedMineContainer test 작성

--- React-Zero/지뢰찾기/src/SelectedMine.jsx
+++ React-Zero/지뢰찾기/src/SelectedMine.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function SelectedMine({ selectedMine }) {
-    return (
-        <h3>! : {selectedMine}개</h3>
-    )
+  return (
+    <h3>! : {selectedMine}개</h3>
+  )
 }
---
+++ React-Zero/지뢰찾기/src/SelectedMineContainer.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import SelectedMineContainer from './SelectedMineContainer';
+
+jest.mock('react-redux');
+
+describe('selectedMineContainer', () => {
+  useSelector(selector => selector({ selectedMine: 10 }))
+
+  it('지뢰를 가져와서 보여준다.', () => {
+    const { queryByText } = render(<SelectedMineContainer />)
+
+    expect(queryByText(/10/)).toBeInTheDocument();
+  });
+});
